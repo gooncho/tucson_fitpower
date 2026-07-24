8e23e6e2124c95210b1a13744e5cdca857bf42c3
Nico-kaiser10 <anmaster.trujillo71@gmail.com>
2026-07-24 17:50:10 -0300

    Se creo el apartado de registro con su respectivo estilo y logica en JS

--- front/dist/JS/script.js
+++ front/dist/JS/script.js
@@ -13,28 +13,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const btnRegistro = document.getElementById('irARegistro');
     const btnLogin = document.getElementById('irALogin');
 
-    // Atrapamos el contenedor que se desliza
     const contenedorDeslizable = document.getElementById('contenedorDeslizable');
 
     const panelGuardado = sessionStorage.getItem('vistaActiva');
 
     if (panelGuardado === 'registro') {
-        // 1. Apagamos temporalmente la animación CSS
+       
         contenedorDeslizable.style.transition = 'none';
 
-        // 2. Ejecutamos el clic para cambiar de panel
         btnRegistro.click();
 
-        // 3. Forzamos al navegador a aplicar el cambio al instante (Reflow)
         void contenedorDeslizable.offsetHeight;
-
-        // 4. Volvemos a encender la animación (restaura el CSS original)
+      
         setTimeout(() => {
             contenedorDeslizable.style.transition = '';
         }, 10);
     }
 
-    // --- Lógica para guardar la posición al hacer clic ---
     if (btnRegistro) {
         btnRegistro.addEventListener('click', function () {
             sessionStorage.setItem('vistaActiva', 'registro');
